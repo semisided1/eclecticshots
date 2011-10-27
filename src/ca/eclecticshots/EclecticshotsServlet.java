5c7b0652f3a37d0e7d77898ed56647aa2e7ff1aa
semisided1 <dirtslayer@gmail.com>
2011-10-27 03:11:02 -0600

    elcipse project minus jar files in lib

---
+++ src/ca/eclecticshots/EclecticshotsServlet.java
@@ -0,0 +1,13 @@
+package ca.eclecticshots;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class EclecticshotsServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
